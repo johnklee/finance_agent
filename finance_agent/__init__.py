7a82cd2065caba6c229e080d111a611fde673399
johnklee <puremonkey2001@yahoo.com.tw>
2026-08-27 11:31:40 +0000

    Publish v0.1.4

--- finance_agent/__init__.py
+++ finance_agent/__init__.py
@@ -2,7 +2,7 @@
 
 from finance_agent.agent import root_agent
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __authors__ = "Kevin We, John"
 
 __all__ = ["root_agent", "__version__", "__authors__"]
